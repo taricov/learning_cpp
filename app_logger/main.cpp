9f6891806c2d6dbbcabbba198b9d466ab2d25e68
TaricOv <tareq.ouf@gmail.com>
2023-04-24 21:30:52 +0200

    first version of the logger newbie lvl

--- app_logger/main.cpp
+++ app_logger/main.cpp
@@ -1,13 +1,41 @@
 #include <cmath>
 #include <iostream>
 
-class log{
-  
+class Log{
+
+public:
+    const int LogLevelError = 0;
+    const int LogLevelWarning = 1;
+    const int LogLevelInfo = 2;
+
+private:
+    int m_LogLevel = LogLevelInfo;
+
+public:
+     void SetLevel(int level) {
+        m_LogLevel = level;
+     }
+     void Error(const char* message) {
+        if(m_LogLevel >= LogLevelError)    
+            std::cout << "[Error]: " << message << std::endl;
+     };
+     void Warn(const char* message) {
+        if(m_LogLevel >= LogLevelWarning)
+            std::cout << "[Warning]: " << message << std::endl;
+     };
+     void Info(const char* message) {
+        if(m_LogLevel >= LogLevelInfo)
+            std::cout << "[Info]: " << message << std::endl;
+     };
 };
 
 int main(){
  
- std::cout << "log" << std::endl;
+ Log log;
+ log.SetLevel(log.LogLevelError);
+ log.Warn("Hello!!");
+ log.Error("Hello!!");
+ log.Info("Hello!!");
 
 };
 
